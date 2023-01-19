8350ad30eb56d3dc40322ddbdfdd0c0f51b2c185
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-19 12:04:22 -0500

    Feature: set up database schema.

---
+++ DapperWorkshop.Sql/dbo/Tables/Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[Name] NVARCHAR(500) NOT NULL,
+	[EmployeeCode] VARCHAR(4) NOT NULL,
+	[Email] VARCHAR(255) NOT NULL,
+	[Age] INT NOT NULL,
+	[HiredAt] DATETIME NOT NULL,
+	[FiredAt] DATETIME NULL
+)
