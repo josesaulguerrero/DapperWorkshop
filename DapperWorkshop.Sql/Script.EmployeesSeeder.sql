858d81ce67171cbebfbfe602f3c83d20f528af1c
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-19 12:23:09 -0500

    Feature: add Employees seeder for the database.

---
+++ DapperWorkshop.Sql/Script.EmployeesSeeder.sql
@@ -0,0 +1,8 @@
+﻿IF NOT EXISTS (select 1 from dbo.[Employee])
+BEGIN 
+	INSERT INTO dbo.[Employee] 
+		(Name, EmployeeCode, Email, Age, HiredAt)
+	VALUES 
+		('Jose', '1223', 'jose.guerrero@dapper.com', 19, GETDATE()),
+		('Tanya', '1452', 'tanya.sereda@dapper.com', 21, GETDATE());
+END
