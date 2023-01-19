7899d7246b60b4657b1e8c3292d5c91ed9f5f255
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-19 16:06:00 -0500

    Feature: add EmployeeDataAccess class.

---
+++ DapperWorkshop.DataAccess/Data/EmployeeDataAccess.cs
@@ -0,0 +1,52 @@
+﻿using DapperWorkshop.DataAccess.DBAccess;
+using DapperWorkshop.DataAccess.Entities;
+
+namespace DapperWorkshop.DataAccess.Data;
+
+public class EmployeeDataAccess
+{
+    private readonly ISqlDataAccess _db;
+
+    public EmployeeDataAccess(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<Employee>> GetAllEmployeesAsync() =>
+        _db.SelectAsync<Employee, dynamic>("dbo.SPEmployee_SelectAll", new { });
+
+    public async Task<Employee?> GetEmployeeByIdAsync(int id)
+    {
+        IEnumerable<Employee> results = await _db.SelectAsync<Employee, dynamic>(
+            "dbo.SPEmployee_SelectByPK",
+            new { Id = id }
+        );
+
+        return results.FirstOrDefault();
+    }
+
+    public Task InsertEmployeeAsync(Employee employee) =>
+        _db.SaveAsync<dynamic>(
+            "dbo.SPEmployee_Insert",
+            new
+            {
+                employee.Name,
+                employee.EmployeeCode,
+                employee.Email,
+                employee.Age,
+                employee.HiredAt
+            }
+        );
+
+    public Task UpdateEmployeeAsync(Employee employee) =>
+        _db.SaveAsync<dynamic>(
+            "dbo.SPEmployee_UpdateByPK",
+            employee
+        );
+
+    public Task DeleteEmployeeAsync(int id) =>
+        _db.SaveAsync<dynamic>(
+            "dbo.SPEmployee_DeleteByPK",
+            new { Id = id }
+        );
+}
--- DapperWorkshop.DataAccess/Entities/Employee.cs
+++ DapperWorkshop.DataAccess/Entities/Employee.cs
@@ -1,6 +1,6 @@
 ﻿namespace DapperWorkshop.DataAccess.Entities;
 
-internal class Employee
+public class Employee
 {
     public int Id { get; set; }
     public string Name { get; set; }
