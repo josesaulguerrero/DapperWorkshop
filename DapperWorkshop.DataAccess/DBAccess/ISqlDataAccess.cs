1e8b3f1ecee36d310bfc146b6f0481267bb37baa
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-19 15:21:31 -0500

    Feature: add SqlDataAccess and ISqlDataAccess.

---
+++ DapperWorkshop.DataAccess/DBAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace DapperWorkshop.DataAccess.DBAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task InsertAsync<T>(string storedProcedure, T parameters, string connectionStringName = "Default");
+        Task<IEnumerable<T>> SelectAllAsync<T, K>(string storedProcedure, K parameters, string connectionStringName = "Default");
+    }
+}
---
+++ DapperWorkshop.DataAccess/DBAccess/SqlDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+using Dapper;
+
+using Microsoft.Extensions.Configuration;
+
+namespace DapperWorkshop.DataAccess.DBAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+
+    public async Task<IEnumerable<T>> SelectAllAsync<T, K>(
+        string storedProcedure,
+        K parameters,
+        string connectionStringName = "Default")
+    {
+        IDbConnection dbConnection = OpenDbConnection(connectionStringName);
+
+        IEnumerable<T> results = await dbConnection.QueryAsync<T>(
+            storedProcedure,
+            parameters,
+            commandType: CommandType.StoredProcedure
+        );
+        dbConnection.Dispose();
+        return results;
+    }
+
+    public async Task InsertAsync<T>(
+        string storedProcedure,
+        T parameters,
+        string connectionStringName = "Default")
+    {
+        IDbConnection dbConnection = OpenDbConnection(connectionStringName);
+
+        await dbConnection.QueryAsync<T>(
+            storedProcedure,
+            parameters,
+            commandType: CommandType.StoredProcedure
+        );
+        dbConnection.Dispose();
+    }
+
+    private IDbConnection OpenDbConnection(string connectionStringName)
+    {
+        return new SqlConnection(_configuration.GetConnectionString(connectionStringName));
+    }
+}
