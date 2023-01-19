13210dc23a2d61b320b0842b0e4053ac2328ad11
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-19 14:19:29 -0500

    Feature: add Data Access layer and create Employee entity.

---
+++ DapperWorkshop.DataAccess/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace DapperWorkshop.DataAccess.Entities;
+
+internal class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string EmployeeCode { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+    public DateTime HiredAt { get; set; }
+    public DateTime? FiredAt { get; set; }
+}
